a427518422f5e672129d8b2a2f7add4b7ba32026
Marco Desantes Gutiérrez <marcodes@ucm.es>
2021-09-15 11:36:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+from telethon import TelegramClient, events, sync
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+
+# initialize the recognizer
+r = sr.Recognizer()
+# initialize the microphone
+m = sr.Microphone()
+
+user_id = "%USER_ID%"
+api_id = "%API_ID%"
+api_hash = "%HASH%"
+
+client = TelegramClient('', api_id, api_hash)
+client.start()
+client.iter_dialogs()
+client.get_entity(PeerChannel('%CHANNEL_ID%'))
+
+while True:
+
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+        # read the audio data from the default microphone
+        print('Listening...')
+        audio_data = r.listen(source)
+        print('Parsing...')
+        # convert speech to text
+        text = r.recognize_google(audio_data, language='es-ES')
+        print('Sending...')
+        client.send_message(
+            PeerChannel('%CHANNEL_ID%'),  # to which entity you are forwarding the messages
+            text  # the messages (or message) to forward
+        )
+
+# REQUIRES PYTHON 3.6
+
+# pip install SpeechRecognition pydub
+# pip install pydub
+
+# WINDOWS
+# pip install pyaudio
+
+# Linux
+# sudo apt-get install python-pyaudio python3-pyaudio
+# pip install pyaudio
+
+# MacOS
+# brew install portaudio
+# pip3 install pyaudio
